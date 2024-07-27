2061f4f948652b22d713727c4cba4f1a47794194
weiwx <weiwx@mangdang.com>
2024-07-27 17:55:50 +0800

    [Fix] fix broken reboot continues

--- main/tts.cpp
+++ main/tts.cpp
@@ -1,11 +1,14 @@
 #include "tts.h"
 #include <Arduino.h>
+#ifdef ESP_IDF_VERSION_MAJOR
+#undef ESP_IDF_VERSION_MAJOR
+#endif
+#define ESP_IDF_VERSION_MAJOR 4
 #include <Audio.h>
 
-#define I2S_DOUT 17 // Data Out
-#define I2S_BCLK 16 // Bit Clock
-#define I2S_LRC 15  // Left/Right Clock
-Audio audio;
+#define I2S_DOUT 17  // Data Out
+#define I2S_BCLK 16  // Bit Clock
+#define I2S_LRC 15   // Left/Right Clock
 const String baseURL = "https://api.voicerss.org/?key=2ab6d13d5e3d430a91406018dfa44c93&hl=en-us&src=";
 const String audioFormat = "&c=mp3&f=16khz_8bit_mono";
 
@@ -30,17 +33,17 @@ String encodeText(const String& text) {
 }
 
 void tts(String text) {
-  
-    audio.setPinout(I2S_BCLK, I2S_LRC, I2S_DOUT);
-    //audio.setVolume(21); // 设置音量级别 (0-100)
+  Audio audio;
+  audio.setPinout(I2S_BCLK, I2S_LRC, I2S_DOUT);
+  //audio.setVolume(21); // 设置音量级别 (0-100)
 
-    // 编码文本并过滤掉非字母数字字符
-    String encodedText = encodeText(text);
+  // 编码文本并过滤掉非字母数字字符
+  String encodedText = encodeText(text);
 
-    // 生成音频 URL
-    String audioURL = baseURL + encodedText + audioFormat;
-    Serial.println("生成的URL: " + audioURL);
+  // 生成音频 URL
+  String audioURL = baseURL + encodedText + audioFormat;
+  Serial.println("生成的URL: " + audioURL);
 
-    // 连接并播放音频
-    audio.connecttohost(audioURL.c_str());
+  // 连接并播放音频
+  audio.connecttohost(audioURL.c_str());
 }
